60f97dfbe08086e29263909eabc0289ae773f8a7
Tushar Juneja <tusharjuneja204@gmail.com>
2022-12-15 23:39:47 +0530

    refactor: Remove redundant image assets and libraries
    Delete dummy images and redundant libraries from code base.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 // import styles from '../styles/Home.module.css'
-import aboutImg from './../assets/img/about-img.jpg';
+// import aboutImg from './../assets/img/about-img.jpg';
 
 export default function Home() {
   return (
