06c4aa996f525c1388a7ad7287ab58ad9793313a
Tushar Juneja <tusharjuneja204@gmail.com>
2022-12-16 14:47:03 +0530

    feat: Add banner section to homepage
    The banner section includes the banner image and 2 headings on it.

---
+++ components/homepage/banner.js
@@ -0,0 +1,10 @@
+import styles from './banner.module.css';
+
+export default function Banner() {
+    return (
+        <section className={`d-flex flex-column justify-content-center align-items-center ${styles.banner}`}>
+            <h1 className={styles.heading1} data-aos="fade-down">Something big is coming</h1>
+            <h1 className={styles.heading2} data-aos="fade-up">stay tuned</h1>
+        </section>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Hero from './../components/homepage/hero';
+import Banner from './../components/homepage/banner';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Banner />
     </>
   );  
 }
