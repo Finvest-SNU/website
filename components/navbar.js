92604ac4602dc1b54557ba0404e21f0fc04c4630
Aditya Kotra <kaditya030@gmail.com>
2022-12-22 14:39:42 +0530

    Revert the small test change

--- components/navbar.js
+++ components/navbar.js
@@ -26,7 +26,7 @@ export default function Navbar() {
                             <li><a href="#">Deep Drop Down 1</a></li>
                             <li><a href="#">Deep Drop Down 2</a></li>
                             <li><a href="#">Deep Drop Down 3</a></li>
-                            small change to test
+                            
                             <li><a href="#">Deep Drop Down 4</a></li>
                             <li><a href="#">Deep Drop Down 5</a></li>
                             </ul>
