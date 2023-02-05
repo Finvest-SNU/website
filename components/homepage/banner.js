7417f238f7687a0a31129da26656c46127d78ca9
Tushar Juneja <tusharjuneja204@gmail.com>
2023-02-05 15:34:51 +0530

    refactor: remove redundant code of banner section

--- components/homepage/banner.js
+++ components/homepage/banner.js
@@ -4,10 +4,6 @@ import MobileImg from './../../assets/img/homepage/banner_mobile.png';
 
 export default function Banner() {
     return (
-        // <section id="banner" className={`d-flex flex-column justify-content-center align-items-center ${styles.banner}`}>
-        //     <h1 className={styles.heading1} data-aos="fade-down">Something big is coming</h1>
-        //     <h1 className={styles.heading2} data-aos="fade-up">stay tuned</h1>
-        // </section>
         <section id="banner" className={`d-flex flex-column justify-content-center align-items-center ${styles.banner}`}>
             <img src={Img.src} className={`img-fluid ${styles.bannerImg}`} alt="Banner" />
             <img src={MobileImg.src} className={`img-fluid ${styles.mobileImg}`} alt="Banner" />
