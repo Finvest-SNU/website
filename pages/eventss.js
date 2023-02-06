fe89020fcc1548cf5e144e86a1f06308166497eb
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-02-06 15:01:16 +0530

    Add files via upload

---
+++ pages/eventss.js
@@ -0,0 +1,58 @@
+import styles from "./events.module.css";
+const EventComponent = () => {
+    const styles1 = {
+        marginTop: '40%'
+      };
+      const background={
+        backgroundColor:"black"
+      };
+    return (
+        <div className="container-fluid" style={background}>
+          <div className="row text-center">
+              <div className={`${styles.img1} text-center p-3 p-md-4 card`}>
+                  <div className={styles.text} style={styles1}>
+                      <p></p>
+                      <p></p>
+                      <p className={styles.content}>What is happening around the world A highly-curated platform for creating, collecting and trading unique NFTs. Trade with confidence on</p>
+                  </div>
+              </div>
+              <div className={`${styles.img2} text-center mt-2  p-3 p-md-4 card`}>
+                  <div className={`${styles.text} mt-5 mb-5`}>
+                      <p className={styles.exp}>EXPERIENCE METAVERSE</p>
+                      <p className={styles.finverse}>FINVERSE</p>
+                      <p className={styles.content}>What is happening around the world A highly-curated platform for creating, collecting and trading unique NFTs. Trade with confidence on</p>
+                      <div className={styles.knowContainer}>
+                          <button className={styles.know}><span className={styles.Knowtext}>Know More</span></button>
+                      </div>
+                  </div>
+              </div>
+              <div className={`${styles.img3} text-center mt-2  p-3 p-md-4 card`}>
+                  <div className={`${styles.text} mt-5 mb-5`}>
+                      <p className={styles.exp}>A NEGOTIATION BASED TRADE</p>
+                      <p className={styles.finverse}>FINQUEST</p>
+                      <p className={styles.content}>What is happening around the world A highly-curated platform for creating, collecting and trading unique NFTs. Trade with confidence on</p>
+                      <div className={styles.knowContainer}>
+                          <button className={styles.know}><span className={styles.Knowtext}>Know More</span></button>
+                      </div>
+                  </div>
+              </div>
+              <div className={`${styles.img4} text-center mt-2  p-3 p-md-4 card`}>
+                  <div className={`${styles.text} mt-5 mb-5`}>
+                      <p className={styles.exp}>LEARN STOCK MARKET WITH</p>
+                      <p className={`${styles.finverse2}`}>LEARNERS’ TRIBE</p>
+                      <p className={`${styles.content} mt-1`}>What is happening around the world A highly-curated platform for creating, collecting and trading unique NFTs. Trade with confidence on</p>
+                      <div className={styles.knowContainer}>
+                          <button className={styles.know}><span className={styles.Knowtext}>Know More</span></button>
+                      </div>
+                  </div>
+              </div>
+              <div className={`${styles.bgblack} text-center mt-2 card  p-3 p-md-4`}>
+                  <div className={`${styles.text} mt-5 mb-5`}>
+                      <p className={`text-white ${styles.content1}`}>And many more to come...</p><span className={styles.emoji}>🤑</span>
+                  </div>
+              </div>
+          </div>
+      </div>
+    );
+  };
+export default EventComponent;
