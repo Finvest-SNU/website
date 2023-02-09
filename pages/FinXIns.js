686e6e7a56d44ac086c78337fde342bfc51df46f
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-02-09 12:57:55 +0530

    Add files via upload

---
+++ pages/FinXIns.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import  FinComponent from './finXinss';
+
+
+
+export default function Event() {
+    return (
+        <>
+            < FinComponent />
+        </>
+    );
+}
