9ad969c35f367ade27f58a655d0f8845ede82158
kunal passan <Kunalpassan30@gmail.com>
2023-11-02 11:36:29 +0530

    fix: overlap of pages

--- components/homepage/banner.js
+++ components/homepage/banner.js
@@ -4,9 +4,10 @@ import MobileImg from './../../assets/img/homepage/banner_mobile.png';
 
 export default function Banner() {
     return (
+        
         <section id="banner" className={`d-flex flex-column justify-content-center align-items-center ${styles.banner}`}>
-            <img src={Img.src} className={`img-fluid ${styles.bannerImg}`} alt="Banner" />
-            <img src={MobileImg.src} className={`img-fluid ${styles.mobileImg}`} alt="Banner" />
+            <img src={Img.src} className={`${styles.bannerImg}`} id="bannerimg" alt="Banner" />
+            <img src={MobileImg.src} className={`${styles.mobileImg}`} alt="Banner" />
         </section>
     );
 }
