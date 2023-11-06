12e95ec6354dbd60710f81973111fa533709de16
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-11-06 23:11:48 +0530

    Update team.js

--- pages/team.js
+++ pages/team.js
@@ -314,6 +314,8 @@ const team =() => {
                 <div className={styles.collapseIn}>
                 <p className={styles.teamMemNames}>Viswa<br/></p>
                     <p className={styles.teamMemNames}>Bharathi<br/></p>
+            <p className={styles.teamMemNames}>Jatin K<br/></p>
+            <p className={styles.teamMemNames}>KVS Tarun<br/></p>
                     <p className={styles.teamMemNames}>Aditya Raghuram<br/></p>
                     <p className={styles.teamMemNames}>Deepshika Chand<br/></p>
                     <p className={styles.teamMemNames}>Ishani Singh<br/></p>
@@ -322,7 +324,7 @@ const team =() => {
                     <p className={styles.teamMemNames}>Noor Walia<br/></p>
                     <p className={styles.teamMemNames}>Pritika Gandhi<br/></p>
                     <p className={styles.teamMemNames}>Srishti Khanna<br/></p>
-                    <p className={styles.teamMemNames}>KVS Tarun<br/></p>
+                    
                     <p className={styles.teamMemNames}>Mugdha Gadgil<br/></p>
                     <p className={styles.teamMemNames}>Partth Kant<br/></p>
                     <p className={styles.teamMemNames}>Aakrisht Narang<br/></p>
